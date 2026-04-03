7eee16fea0355771e60a891059595f60655553bc
Gharke-Ram55 <gharkeram55@gmail.com>
2026-04-03 22:18:53 +0530

    final commit

--- fintrack/settings.py
+++ fintrack/settings.py
@@ -71,7 +71,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
