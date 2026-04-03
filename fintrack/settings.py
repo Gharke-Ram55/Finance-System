9b48cc7f0be9e03e4eabbcd3651117e48213eca4
Gharke-Ram55 <gharkeram55@gmail.com>
2026-04-03 22:24:13 +0530

    final commit

--- fintrack/settings.py
+++ fintrack/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-dev-secret-key-change-in-production-zorvyn-fintrack'
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['*']
 
